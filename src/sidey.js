35aaa9ac3ad53ffb2d5443b0af2ab215471aa902
Mark Moffat <mrvautin@gmail.com>
2016-08-18 21:08:54 +0930

    Initial commit

---
+++ src/sidey.js
@@ -0,0 +1,36 @@
+(function($){
+    $.fn.sidey = function(options){
+        var element = this;
+
+        $('.sidey-toggle').click(function(e){
+            toggleSidebar($(this).data('toggle'));
+        });
+        
+        function toggleSidebar(direction){
+            console.log(element);
+            if(direction === 'left'){
+                $(element).toggleClass('sidey-leftactive');
+                $('.sidey-content').toggleClass('sidey-leftpush');
+            }
+
+            if(direction === 'right'){
+                $(element).toggleClass('sidey-rightactive');
+                $('.sidey-content').toggleClass('sidey-rightpush');
+            }
+        }
+
+        $(document).click(function(e){
+            var target = $(e.target);
+            if(!target.closest('a').is('.sidey-toggle')){
+                if(!$(e.target).closest('.sidebar').length){
+                    if($(element).hasClass('sidey-leftactive')){
+                        toggleSidebar('left');
+                    }
+                    if($(element).hasClass('sidey-rightactive')){
+                        toggleSidebar('right');
+                    }
+                }
+            }
+        });
+    };
+}(jQuery));
