becaa9fb1a33918e317be56f8bd536b2129a4587
Mark Moffat <mrvautin@gmail.com>
2016-08-21 10:54:30 +0930

    Changed API (sorry) to allow for multiple sidey instances on a page and added `backgroundBlur` option

--- src/sidey.js
+++ src/sidey.js
@@ -1,35 +1,86 @@
 (function($){
-    $.fn.sidey = function(options){
-        var element = this;
+    $.extend({
+        sidey: function(options){
+            // get the options
+            var settings = $.extend({
+                backgroundBlur: true
+            }, options);
 
-        $('.sidey-toggle').click(function(e){
-            toggleSidebar($(this).data('toggle'));
-        });
+            $('.sidey-toggle').unbind().click(function(e){
+                var element = $($(this).data('element'));
 
-        function toggleSidebar(direction){
-            if(direction === 'left'){
-                $(element).toggleClass('sidey-leftactive');
-                $('.sidey-content').toggleClass('sidey-leftpush');
-            }
+                // if already open then we close it
+                if($(element).hasClass('sidey-leftactive')){
+                    toggleSidebar(element, 'left');
+                    return;
+                }
 
-            if(direction === 'right'){
-                $(element).toggleClass('sidey-rightactive');
-                $('.sidey-content').toggleClass('sidey-rightpush');
-            }
-        }
+                // if already open then we close it
+                if($(element).hasClass('sidey-rightactive')){
+                    toggleSidebar(element, 'right');
+                    return;
+                }
 
-        $(document).click(function(e){
-            var target = $(e.target);
-            if(!target.closest('a').is('.sidey-toggle')){
-                if(!$(e.target).closest('.sidebar').length){
-                    if($(element).hasClass('sidey-leftactive')){
-                        toggleSidebar('left');
+                // close any active sidebars
+                closeSidebars();
+
+                // open the chosen sidebar
+                if($(element).hasClass('sidey-left')){
+                    $(element).addClass('sidey-leftactive');
+                    $('.sidey-content').addClass('sidey-leftpush');
+                    if(settings.backgroundBlur === true){
+                        $('.sidey-content').toggleClass('sidey-blur');
                     }
-                    if($(element).hasClass('sidey-rightactive')){
-                        toggleSidebar('right');
+                }else if($(element).hasClass('sidey-right')){
+                    $(element).toggleClass('sidey-rightactive');
+                    $('.sidey-content').toggleClass('sidey-rightpush');
+                    if(settings.backgroundBlur === true){
+                        $('.sidey-content').toggleClass('sidey-blur');
+                    }
+                }
+            });
+
+            // anything outside of the sidebar and toggle clicked
+            $(document).unbind().click(function(e){
+                if(!$(e.target).hasClass('sidey-toggle')){
+                    if(!$(e.target).closest('.sidey-leftactive').length || $(e.target).closest('.sidey-rightactive').length){
+                        closeSidebars();
+                    }
+                }
+            });
+
+            // close all open sidebars
+            function closeSidebars(){
+                $('.sidey-left').each(function(index){
+                    $(this).removeClass('sidey-leftactive');
+                });
+                $('.sidey-right').each(function(index){
+                    $(this).removeClass('sidey-rightactive');
+                });
+                $('.sidey-content').each(function(index){
+                    $(this).removeClass('sidey-rightpush');
+                    $(this).removeClass('sidey-leftpush');
+                });
+            }
+
+            // toggle a sidebar
+            function toggleSidebar(element, direction){
+                if(direction === 'left'){
+                    $(element).toggleClass('sidey-leftactive');
+                    $('.sidey-content').toggleClass('sidey-leftpush');
+                    if(settings.backgroundBlur === true){
+                        $('.sidey-content').toggleClass('sidey-blur');
+                    }
+                }
+
+                if(direction === 'right'){
+                    $(element).toggleClass('sidey-rightactive');
+                    $('.sidey-content').toggleClass('sidey-rightpush');
+                    if(settings.backgroundBlur === true){
+                        $('.sidey-content').toggleClass('sidey-blur');
                     }
                 }
             }
-        });
-    };
+        }
+    });
 }(jQuery));
